448ab4afa8d3b0bb1cf43048b9e8d78c769cdd89
Your Name <jinathin@gmail.com>
2019-01-28 13:39:14 -0500

    making puzzle pieces

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -2,17 +2,39 @@
 	console.log('fired')
 	// set up the puzzle pieces and boards
 
-	const theButton = document.querySelector("#buttonHolder img");
-	
-	function changeHeadline() {
-		document.querySelector("h1").textContent = "Changing headline with JS.";
-		document.querySelector("p").textContent = "This is the subhead";
-	}
-	// debugger;
-	// it makes the code stops
-	// window.addEventListener("load", changeHeadline);
-	theButton.addEventListener("click", changeHeadline);
-	// load, click, mouse etc are the command to make it work
+	// need a reference to each piecese that we want to create 
+	const thePieces= ["topLeft", "topRight", "bottomLeft", "bottomRight"]
 
-	
+	// get a reference to the drag side 
+	let piecesBoard = document.querySelector(".puzzle-pieces");
+	let puzzleBoard = document.querySelector(".puzzle-board")
+
+	// get a reference to the buttons at the bottom so we can change the puzzle 
+	let puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+
+	// functions go in the middle
+	function createPuzzlePieces(pictureIndex) {
+		// generate images here -> need to make 4 (top left, right bottom left, right)
+		// debugger;
+		// loop through the images refs and generate one for each
+		thePieces.forEach((piece, index) => {
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="puzzle piece">`;
+
+			piecesBoard.innerHTML += newPuzzlePiece;
+		});
+	}
+
+	function resetPuzzlePieces() {
+		// change the current puzzle, regnerate the pieces
+		// debugger;
+		piecesBoard.innerHTML = ""
+		createPuzzlePieces(this.dataset.puzzleref);
+	}
+
+	// event handling goes here
+	puzzleSelectors.forEach(button => button.addEventListener("click", resetPuzzlePieces));
+
+	// call this function to set up / generate the pieces on Load
+	createPuzzlePieces(0);
+
 })();
